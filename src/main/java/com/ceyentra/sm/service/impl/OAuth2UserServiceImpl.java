1d239550727a4d59355e75d0005bff612dd10479
IsharaIcbt <isharaicbt@gmail.com>
2024-07-23 23:40:46 +0530

    updated OAuth2UserServiceImpl

--- src/main/java/com/ceyentra/sm/service/impl/OAuth2UserServiceImpl.java
+++ src/main/java/com/ceyentra/sm/service/impl/OAuth2UserServiceImpl.java
@@ -4,12 +4,16 @@
  */
 package com.ceyentra.sm.service.impl;
 
+import com.ceyentra.sm.entity.AdminEntity;
+import com.ceyentra.sm.entity.StaffEntity;
 import com.ceyentra.sm.entity.UserEntity;
 import com.ceyentra.sm.exception.CustomOauthException;
+import com.ceyentra.sm.repository.AdminRepo;
+import com.ceyentra.sm.repository.StaffRepo;
 import com.ceyentra.sm.repository.UserRepo;
 import com.ceyentra.sm.service.OAuth2UserService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -28,36 +32,57 @@ import java.util.Optional;
 @Service(value = "userService")
 @Log4j2
 @Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+@RequiredArgsConstructor
 public class OAuth2UserServiceImpl implements OAuth2UserService, UserDetailsService {
 
     private final UserRepo userRepository;
-
-    @Autowired
-    public OAuth2UserServiceImpl(UserRepo userRepository) {
-        this.userRepository = userRepository;
-    }
+    private final AdminRepo adminRepo;
+    private final StaffRepo staffRepo;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         log.info("starting function loadUserByUsername @Param username : {}", username);
         try {
-
+            // find authentication
             Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-            User user1 = (User) authentication.getPrincipal();
-            System.out.println(user1.getUsername());
-            System.out.println(authentication.getAuthorities());
-            System.out.println(authentication.getCredentials());
+            User authenticationPrincipal = (User) authentication.getPrincipal();
 
-            Optional<UserEntity> byEmail = userRepository.findByEmail(username);
+            switch (authenticationPrincipal.getUsername()) {
+                case "ADMIN":
+                    Optional<AdminEntity> admin = adminRepo.findByUsername(username);
 
-            if (!byEmail.isPresent())
-                throw new CustomOauthException("Invalid Credentials.");
+                    if (!admin.isPresent()) {
+                        throw new CustomOauthException("Invalid Credentials.");
+                    }
 
-            UserEntity user = byEmail.get();
-            log.info("Issuing tokens for user with username : {} password : {}", user.getEmail(), user.getPassword());
-            return new org.springframework.security.core.userdetails.User(
-                    user.getEmail(), user.getPassword(), getAuthority(user.getUserRole().name())
-            );
+                    return new org.springframework.security.core.userdetails.User(
+                            admin.get().getEmail(), admin.get().getPassword(),
+                            getAuthority(admin.get().getUserRole().name())
+                    );
+
+                case "USER":
+                    Optional<UserEntity> customer = userRepository.findByEmail(username);
+
+                    if (!customer.isPresent())
+                        throw new CustomOauthException("Invalid Credentials.");
+
+                    return new org.springframework.security.core.userdetails.User(
+                            customer.get().getEmail(), customer.get().getPassword(),
+                            getAuthority(customer.get().getUserRole().name()));
+
+                case "STAFF":
+                    Optional<StaffEntity> staff = staffRepo.findByEmail(username);
+
+                    if (!staff.isPresent())
+                        throw new CustomOauthException("Invalid Credentials.");
+
+                    return new org.springframework.security.core.userdetails.User(
+                            staff.get().getEmail(), staff.get().getPassword(),
+                            getAuthority(staff.get().getUserRole().name()));
+
+                default:
+                    throw new CustomOauthException("Invalid Credentials.");
+            }
 
         } catch (Exception e) {
             log.error("function loadUserByUsername {}", e.getMessage(), e);

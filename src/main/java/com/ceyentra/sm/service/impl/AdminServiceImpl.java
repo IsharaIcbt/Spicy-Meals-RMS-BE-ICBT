840d62d45d17d2f19c3d53e6969724f8d9c53246
IsharaIcbt <isharaicbt@gmail.com>
2024-08-04 16:11:22 +0530

    updated AdminServiceImpl

--- src/main/java/com/ceyentra/sm/service/impl/AdminServiceImpl.java
+++ src/main/java/com/ceyentra/sm/service/impl/AdminServiceImpl.java
@@ -4,12 +4,14 @@ import com.ceyentra.sm.dto.web.request.SaveAdminReqDTO;
 import com.ceyentra.sm.entity.AdminEntity;
 import com.ceyentra.sm.entity.RestaurantEntity;
 import com.ceyentra.sm.entity.StaffEntity;
+import com.ceyentra.sm.entity.UserEntity;
 import com.ceyentra.sm.enums.CommonStatus;
 import com.ceyentra.sm.enums.UserRole;
 import com.ceyentra.sm.exception.ApplicationServiceException;
 import com.ceyentra.sm.repository.AdminRepo;
 import com.ceyentra.sm.repository.RestaurantRepo;
 import com.ceyentra.sm.repository.StaffRepo;
+import com.ceyentra.sm.repository.UserRepo;
 import com.ceyentra.sm.service.AdminService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -29,6 +31,7 @@ public class AdminServiceImpl implements AdminService {
 
     private final AdminRepo adminRepo;
     private final StaffRepo staffRepo;
+    private final UserRepo userRepo;
     private final RestaurantRepo restaurantRepo;
 
     @Override
@@ -45,6 +48,12 @@ public class AdminServiceImpl implements AdminService {
             Optional<StaffEntity> staffByEmail = staffRepo.findByEmail(adminReqDTO.getEmail());
 
             if (staffByEmail.isPresent() && !staffByEmail.get().getStatus().equals(CommonStatus.DELETED)) {
+                throw new ApplicationServiceException(200, false, "Email is already in use");
+            }
+
+            Optional<UserEntity> userByEmail = userRepo.findByEmail(adminReqDTO.getEmail());
+
+            if (userByEmail.isPresent() && !userByEmail.get().getStatus().equals(CommonStatus.DELETED)) {
                 throw new ApplicationServiceException(200, false, "Email is already in use");
             }
 

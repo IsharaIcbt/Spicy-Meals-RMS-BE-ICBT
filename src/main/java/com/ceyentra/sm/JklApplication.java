a400de7f5fa7f1c75b9ae00bd5c0c67145944d58
IsharaIcbt <isharaicbt@gmail.com>
2024-08-12 21:41:36 +0530

    load main app env Configure loader class

--- src/main/java/com/ceyentra/sm/JklApplication.java
+++ src/main/java/com/ceyentra/sm/JklApplication.java
@@ -1,5 +1,6 @@
 package com.ceyentra.sm;
 
+import com.ceyentra.sm.config.EnvConfigLoader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -7,6 +8,8 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import java.io.IOException;
 
 @SpringBootApplication
 @EnableAsync
@@ -19,7 +22,10 @@ public class JklApplication extends SpringBootServletInitializer {
 		return application.sources(JklApplication.class);
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+
+		// Load .env file
+		EnvConfigLoader.loadEnv();
 		SpringApplication.run(JklApplication.class, args);
 	}
 

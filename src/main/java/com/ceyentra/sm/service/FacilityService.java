95195ec598cdd5b935df0fb5fde980247c99b65c
IsharaIcbt <isharaicbt@gmail.com>
2024-08-13 00:09:37 +0530

    invoke service method

---
+++ src/main/java/com/ceyentra/sm/service/FacilityService.java
@@ -0,0 +1,9 @@
+package com.ceyentra.sm.service;
+
+import com.ceyentra.sm.dto.web.response.FacilityCommonResDTO;
+
+import java.util.List;
+
+public interface FacilityService {
+    List<FacilityCommonResDTO> findAllFacilities();
+}

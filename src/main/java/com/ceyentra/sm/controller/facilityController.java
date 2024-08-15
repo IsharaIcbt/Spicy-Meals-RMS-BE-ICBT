f391107c359f8eb6b6f7db214576fa05ae7366ff
IsharaIcbt <isharaicbt@gmail.com>
2024-08-15 22:48:17 +0530

    update api endpoint

--- src/main/java/com/ceyentra/sm/controller/facilityController.java
+++ src/main/java/com/ceyentra/sm/controller/facilityController.java
@@ -4,12 +4,19 @@ import com.ceyentra.sm.config.throttling_config.Throttling;
 import com.ceyentra.sm.dto.common.CommonResponseDTO;
 import com.ceyentra.sm.dto.common.ResponseDTO;
 import com.ceyentra.sm.dto.web.request.SaveFacilityReqDTO;
+import com.ceyentra.sm.enums.CommonFunctionalFrequency;
+import com.ceyentra.sm.enums.CommonStatus;
+import com.ceyentra.sm.enums.FacilityType;
 import com.ceyentra.sm.service.FacilityService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Date;
 
 @RestController
 @CrossOrigin
@@ -42,17 +49,51 @@ public class facilityController {
         );
     }
 
-    @Throttling(timeFrameInSeconds = 60, calls = 20)
-    @PostMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<CommonResponseDTO> registerFacility(@RequestBody SaveFacilityReqDTO saveFacilityReqDTO) {
+    @PostMapping(value = "/save", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<CommonResponseDTO> registerFacility(
+            @RequestParam("id") Long id,
+            @RequestParam("restaurantId") Long restaurantId,
+            @RequestParam("name") String name,
+            @RequestParam(value = "imgURL" , required = false) MultipartFile imgURL,
+            @RequestParam("description") String description,
+            @RequestParam("frequency") CommonFunctionalFrequency frequency,
+            @RequestParam("reservedDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) Date reservedDate,
+            @RequestParam("start") String start,
+            @RequestParam("close") String close,
+            @RequestParam("weekDays") String weekDays,
+            @RequestParam("maxParticipantCount") Integer maxParticipantCount,
+            @RequestParam("price") Float price,
+            @RequestParam("discount") Float discount,
+            @RequestParam("facilityType") FacilityType facilityType,
+            @RequestParam("availability") CommonStatus availability
+    ) {
+        // Your logic here
+        SaveFacilityReqDTO saveFacilityReqDTO = SaveFacilityReqDTO.builder()
+                .id(id).restaurantId(restaurantId)
+                .name(name)
+                .imgURL(imgURL)
+                .description(description)
+                .frequency(frequency)
+                .reservedDate(reservedDate)
+                .start(start)
+                .close(close)
+                .weekDays(weekDays)
+                .maxParticipantCount(maxParticipantCount)
+                .price(price)
+                .discount(discount)
+                .facilityType(facilityType)
+                .availability(availability)
+                .build();
+
         facilityService.saveFacility(saveFacilityReqDTO);
         return new ResponseEntity<>(
                 CommonResponseDTO.builder()
                         .success(true)
                         .message("Facility successfully registered.")
-                        .build()
-                , HttpStatus.OK
+                        .build(),
+                HttpStatus.OK
         );
     }
+
 
 }

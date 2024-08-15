82b3736bf9fbefaffc78b69f7252e03b26e1ffe9
IsharaIcbt <isharaicbt@gmail.com>
2024-08-15 22:47:52 +0530

    update FacilityCommonResDTO

--- src/main/java/com/ceyentra/sm/dto/web/response/FacilityCommonResDTO.java
+++ src/main/java/com/ceyentra/sm/dto/web/response/FacilityCommonResDTO.java
@@ -15,6 +15,7 @@ import java.util.Date;
 public class FacilityCommonResDTO {
     Long id;
     Long restaurantId;
+    String restaurantName;
     String name;
     String imgURL;
     String description;
@@ -30,4 +31,24 @@ public class FacilityCommonResDTO {
     CommonStatus availability;
     Date createdDate;
     Date updatedDate;
+
+    public FacilityCommonResDTO(Long id, Long restaurantId, String name, String imgURL, String description, CommonFunctionalFrequency frequency, Date reservedDate, String start, String close, String weekDays, Integer maxParticipantCount, Float price, Float discount, FacilityType facilityType, CommonStatus availability, Date createdDate, Date updatedDate) {
+        this.id = id;
+        this.restaurantId = restaurantId;
+        this.name = name;
+        this.imgURL = imgURL;
+        this.description = description;
+        this.frequency = frequency;
+        this.reservedDate = reservedDate;
+        this.start = start;
+        this.close = close;
+        this.weekDays = weekDays;
+        this.maxParticipantCount = maxParticipantCount;
+        this.price = price;
+        this.discount = discount;
+        this.facilityType = facilityType;
+        this.availability = availability;
+        this.createdDate = createdDate;
+        this.updatedDate = updatedDate;
+    }
 }

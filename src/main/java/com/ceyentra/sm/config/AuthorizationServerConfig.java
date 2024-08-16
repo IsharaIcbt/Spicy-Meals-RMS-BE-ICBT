68222fa0d3fbb4827574bcb2335824c5797f7fdc
IsharaIcbt <isharaicbt@gmail.com>
2024-08-17 00:33:39 +0530

    added AuthorizationServerConfig

--- src/main/java/com/ceyentra/sm/config/AuthorizationServerConfig.java
+++ src/main/java/com/ceyentra/sm/config/AuthorizationServerConfig.java
@@ -13,9 +13,13 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+import java.util.Arrays;
 
 import static com.ceyentra.sm.constant.OAuth2Constant.*;
 
@@ -25,11 +29,13 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
     private final AuthenticationManager authenticationManager;
     private final BCryptPasswordEncoder encoder;
+    private final CustomTokenEnhancer customTokenEnhancer;
 
     @Autowired
-    public AuthorizationServerConfig(AuthenticationManager authenticationManager, BCryptPasswordEncoder encoder) {
+    public AuthorizationServerConfig(AuthenticationManager authenticationManager, BCryptPasswordEncoder encoder, CustomTokenEnhancer customTokenEnhancer) {
         this.authenticationManager = authenticationManager;
         this.encoder = encoder;
+        this.customTokenEnhancer = customTokenEnhancer;
     }
 
     @Override
@@ -63,7 +69,10 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
+        TokenEnhancerChain enhancerChain = new TokenEnhancerChain();
+        enhancerChain.setTokenEnhancers(Arrays.asList(tokenEnhancer(), accessTokenConverter()));
         endpoints.tokenStore(tokenStore())
+                .tokenEnhancer(enhancerChain)
                 .authenticationManager(authenticationManager)
                 .accessTokenConverter(accessTokenConverter())
                 .pathMapping("/oauth/token", "/v1/oauth/token")
@@ -74,6 +83,18 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
                         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new CustomOauthException((exception.getMessage()) != null ?
                                 exception.getMessage() : "Sorry, something went wrong."));
                 });
+
+//        endpoints.tokenStore(tokenStore())
+//                .authenticationManager(authenticationManager)
+//                .accessTokenConverter(accessTokenConverter())
+//                .pathMapping("/oauth/token", "/v1/oauth/token")
+//                .exceptionTranslator(exception -> {
+//                    if (exception instanceof InvalidGrantException)
+//                        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new CustomOauthException("Invalid credentials."));
+//                    else
+//                        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new CustomOauthException((exception.getMessage()) != null ?
+//                                exception.getMessage() : "Sorry, something went wrong."));
+//                });
     }
 
     @Bean
@@ -88,4 +109,8 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
         return new JwtTokenStore(accessTokenConverter());
     }
 
+    @Bean
+    public TokenEnhancer tokenEnhancer() {
+        return customTokenEnhancer;
+    }
 }

1c998a60b7b304d8588a2ce2b2b50c1a68dd6674
IsharaIcbt <isharaicbt@gmail.com>
2024-08-25 15:34:58 +0530

    added MealOrderDetailRepo, MealOrderRepo

--- src/main/java/com/ceyentra/sm/repository/MealOrderRepo.java
+++ src/main/java/com/ceyentra/sm/repository/MealOrderRepo.java
@@ -3,5 +3,9 @@ package com.ceyentra.sm.repository;
 import com.ceyentra.sm.entity.MealOrderEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MealOrderRepo extends JpaRepository<MealOrderEntity, Long> {
+
+    List<MealOrderEntity> findByUserEntityId(Long id);
 }

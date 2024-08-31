4d56a4302bf270fbc345091a423fb5360f0579a3
IsharaIcbt <isharaicbt@gmail.com>
2024-09-01 01:11:18 +0530

    updated MealOrderRepo and TableReservationRepo

--- src/main/java/com/ceyentra/sm/repository/MealOrderRepo.java
+++ src/main/java/com/ceyentra/sm/repository/MealOrderRepo.java
@@ -2,10 +2,14 @@ package com.ceyentra.sm.repository;
 
 import com.ceyentra.sm.entity.MealOrderEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface MealOrderRepo extends JpaRepository<MealOrderEntity, Long> {
 
     List<MealOrderEntity> findByUserEntityId(Long id);
+
+    @Query("FROM MealOrderEntity WHERE MealOrderEntity.id=:id")
+    List<MealOrderEntity> findByIdV2(Long id);
 }
--- src/main/java/com/ceyentra/sm/repository/TableReservationRepo.java
+++ src/main/java/com/ceyentra/sm/repository/TableReservationRepo.java
@@ -2,6 +2,7 @@ package com.ceyentra.sm.repository;
 
 import com.ceyentra.sm.entity.TableReservationEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +11,7 @@ import java.util.List;
 public interface TableReservationRepo extends JpaRepository<TableReservationEntity, Long> {
 
     List<TableReservationEntity> findTableReservationEntityByCustomerId(Long id);
+
+    @Query("FROM TableReservationEntity WHERE TableReservationEntity.id=:id")
+    List<TableReservationEntity> findByIdV2(Long id);
 }
